1dcf3c6743488b0ae2e1c4b138e11184196236c6
liu-sun <sunliu@yxnu.edu.cn>
2022-02-20 19:35:43 +0800

    format pretty

--- StemBranch.java
+++ StemBranch.java
@@ -1,12 +1,18 @@
-public class StemBranch {
-    public static String stemBranch(int year) {
-        String[] stem = { "甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸" };
-        String[] branch = { "子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥" };
-        return stem[(year - 4) % 10] + branch[(year - 4) % 12];
-    }
+package stembranch;
 
-    public static void main(String[] args) {
-        int year = Integer.parseInt(args[0]);
-        System.out.println(stemBranch(year));
-    }
+import java.util.Scanner;
+
+public class StemBranch {
+	public static String stembranch(int year) {
+		String[] stem = { "甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸" };
+		String[] branch = { "子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥" };
+		return stem[(year - 4) % 10] + branch[(year - 4) % 12];
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int year = scanner.nextInt();
+		scanner.close();
+		System.out.println(stembranch(year));
+	}
 }
