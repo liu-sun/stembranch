5c81261536a36eaf05e994f1eb3433d53afb24f5
liu-sun <46041656+liu-sun@users.noreply.github.com>
2022-03-05 20:25:56 +0800

    Update stembranch.c

--- stembranch.c
+++ stembranch.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void stembranch(int year)
-{
+void stembranch(int year) {
     char *stem[] = {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"};
     char *branch[] = {"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"};
     printf("%s%s\n", stem[(year - 4) % 10], branch[(year - 4) % 12]);
 }
 
-int main(int argc, char *argv[])
-{
-    stembranch(2022);
+int main(int argc, char *argv[]) {
+    stembranch(atoi(argv[1]));
     return 0;
 }
