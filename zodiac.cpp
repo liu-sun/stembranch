98a2b87209dd2d102730cb8423951c43071c6060
liu-sun <46041656+liu-sun@users.noreply.github.com>
2022-03-06 14:44:07 +0800

    Create zodiac.cpp

---
+++ zodiac.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+ const char* zodiac[12] = { "鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪" };
+ int year;
+ cin >> year;
+ cout <<  zodiac[(year-4)%12] << endl;
+ return 0;
+}
